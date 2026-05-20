12258e21319b627e4de89cdcaec76feb6f351011
bhallavinod <bhallavinod@gmail.com>
2026-05-20 08:52:57 +0530

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,24 @@
+const CACHE = 'cymatics-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting(); // ← activate immediately, don't wait
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim(); // ← take control of all open tabs immediately
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
